7c10f08c7fe2f509b14351693343f29d9894e708
Qiuchenyang123 <38029901+Qiuchenyang123@users.noreply.github.com>
2020-12-25 16:55:55 +0800

    Create BasicLayout.tsx
    20201225——BasicLayout.tsx

--- src/layouts/BasicLayout.tsx
+++ src/layouts/BasicLayout.tsx
@@ -1,6 +1,7 @@
 import React, {useEffect} from 'react';
 import 'antd-mobile/dist/antd-mobile.css';
 import {Location, Dispatch, connect} from 'umi';
+import '../static/style/base.css';
 import {ConnectState, ConnectProps, UserModelState} from '@/models/connect';
 import BottomNav from "@/components/BottomNav/BottomNav";
 import SecurityLayout from "@/layouts/SecurityLayout";
@@ -24,17 +25,11 @@ const BasicLayout: React.FC<BasicLayoutProps> = (props) => {
     }
   }, [])
 
-  console.log('props', props)
   return (
     <div className="BasicLayoutWrap">
-      <SecurityLayout user={user} key={'123'}>
-          <article>{children}</article>
-      </SecurityLayout>
-      <BottomNav pathname={pathname}/>
+      <article>{children}</article>
     </div>
   );
 }
 
 export default connect(({user}: ConnectState) => ({user}))(BasicLayout);
-
-
