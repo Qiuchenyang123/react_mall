255ddee9f508edc4d421ba3c4283311d3de8890b
Qiuchenyang123 <38029901+Qiuchenyang123@users.noreply.github.com>
2020-12-25 17:11:54 +0800

    Create index.tsx
    20201225——Login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,11 +1,33 @@
-import React from 'react';
-import styles from './index.less';
+import React, {useEffect} from 'react';
+import LoginForm from '@/page/login/LoginForm';
+import {Location, Dispatch, connect, Redirect} from 'umi';
+import {ConnectState, ConnectProps, UserModelState} from '@/models/connect';
 
-export default () => {
-  console.log('login')
+interface LoginProps extends ConnectProps {
+  user: UserModelState
+}
+
+const Login: React.FC<LoginProps> = (props) => {
+  const {location, dispatch, user} = props
+  const {userid} = location;
+  const isLogin = !!userid;
+
+  if (isLogin) {
+      const {from = '/'} = location.state || {};
+      return <Redirect to={from} />
+  }
+
+  const loginHandle = (account: String, password: String) =>{
+      dispatch({ type: 'user/login', payload: {password, name: account} });
+  }
+
   return (
-    <div>
-      <h1 className={styles.title}>Page login/index</h1>
+    <div className="loginWrap">
+      <div className="formWrap">
+        <LoginForm loginHandle={loginHandle}/>
+      </div>
     </div>
   );
 }
+
+export default connect(({user}: ConnectState) => ({user}))(Login);
